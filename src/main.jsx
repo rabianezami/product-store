3e70492af62b4d754af4bee016386bee238c82b6
rabia zia nezami <rabianezami786@gmail.com>
2026-04-14 23:21:51 +0430

    core: wrap root App with AppProviders

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AppProviders } from './app/Providers.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>
+);
