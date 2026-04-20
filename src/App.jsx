d4feae5099f27fc022c749245b3f25099f928ef3
rabia zia nezami <rabianezami786@gmail.com>
2026-04-20 23:58:50 +0430

    Use sortSelect to see how it looks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from './components/common/Navbar'
+import SortSelect from './components/common/SortSelect'
 
 function App() {
 
   return (
     <>
      <Navbar />
+     <SortSelect />
     
     </>
   )
