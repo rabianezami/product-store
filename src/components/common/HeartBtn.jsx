4dee740380feb9f45cbab68c861ea534a4b21798
rabia zia nezami <rabianezami786@gmail.com>
2026-04-20 23:04:21 +0430

    Add reuseable HeaartBtn component

---
+++ src/components/common/HeartBtn.jsx
@@ -0,0 +1,18 @@
+import { Heart } from "lucide-react";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+export default function HeartBtn({ active, variant = "default", onClick }) {
+  return (
+    <Button
+      onClick={onClick}
+      size="icon"
+      className={cn(
+        variant === "card" && "text-white bg-black/60 hover:bg-black/80",
+        variant === "navbar" && "text-black"
+      )}
+    >
+      <Heart className={active ? "fill-red-500 text-red-500" : ""} />
+    </Button>
+  );
+}
