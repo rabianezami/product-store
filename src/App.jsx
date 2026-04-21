2e4212f21df6947145e0368f1e17a3cf57cad13e
rabia zia nezami <rabianezami786@gmail.com>
2026-04-21 23:46:11 +0430

    Setup main styles and use Toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
+import Container from './components/common/Container'
 import Navbar from './components/common/Navbar'
-import SortSelect from './components/common/SortSelect'
+import Toolbar from './components/common/Toolbar'
 
 function App() {
 
+  const [cartOpen, setCartOpen] = useState(false)
+
   return (
-    <>
-     <Navbar />
-     <SortSelect />
-    
-    </>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-write'>
+      <Navbar onOpenCart={() => setCartOpen(true)} />
+      <main className='py-8'>
+        <Container>
+          <Toolbar />
+        </Container>
+      </main>
+    </div>
   )
 }
 
