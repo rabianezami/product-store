9f6278188f94fff7da7118ac1f5dff45962c1daa
rabia zia nezami <rabianezami786@gmail.com>
2026-04-23 23:13:27 +0430

    Call components and set main space

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { useState } from 'react'
 import './App.css'
 import Container from './components/common/Container'
 import Navbar from './components/common/Navbar'
-import Toolbar from './components/common/Toolbar'
+import ProductList from './features/products/components/ProductList'
+
 
 function App() {
 
   const [cartOpen, setCartOpen] = useState(false)
 
+ 
   return (
-    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-write'>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
       <Navbar onOpenCart={() => setCartOpen(true)} />
       <main className='py-8'>
         <Container>
-          <Toolbar />
+          <ProductList />
         </Container>
       </main>
     </div>
