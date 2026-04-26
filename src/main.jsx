12a7103eac1c7bf23fe261f6c03a12789d8aeea1
rabia zia nezami <rabianezami786@gmail.com>
2026-04-26 19:03:40 +0430

    Wrap root with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppProviders } from './app/Providers.jsx'
-
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
+    <BrowserRouter>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </BrowserRouter>
   </StrictMode>
 );
