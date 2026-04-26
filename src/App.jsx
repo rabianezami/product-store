bb0899fdc1425f0a9191b93bc134491ef3b6edd8
rabia zia nezami <rabianezami786@gmail.com>
2026-04-26 23:47:20 +0430

    Add routing for product list and detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Container from './components/common/Container'
 import Navbar from './components/common/Navbar'
 import ProductList from './features/products/components/ProductList'
 import CartDrawer from './features/cart/components/CartDrawer'
+import ProductDetaile from './features/products/pages/ProductDetail'
 
 
 function App() {
 
   const [cartOpen, setCartOpen] = useState(false)
 
- 
+
   return (
-    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
-      <Navbar onOpenCart={() => setCartOpen(true)} />
+      <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+        <Navbar onOpenCart={() => setCartOpen(true)} />
         <CartDrawer
           open={cartOpen}
           onClose={() => setCartOpen(false)}
         />
-      <main className='py-8'>
-        <Container>
-          <ProductList />
-        </Container>
-      </main>
-    </div>
+        <main className='py-8'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<ProductList />} />
+              <Route path='/product/:id' element={<ProductDetaile />}/>
+            </Routes>
+          </Container>
+        </main>
+      </div>
   )
 }
 
