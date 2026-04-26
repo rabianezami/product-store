b5dc7d59a1cf819dc427badbc90d98a7098a2d0c
rabia zia nezami <rabianezami786@gmail.com>
2026-04-26 19:47:18 +0430

    Integrate CartDrawer into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Container from './components/common/Container'
 import Navbar from './components/common/Navbar'
 import ProductList from './features/products/components/ProductList'
+import CartDrawer from './features/cart/components/CartDrawer'
 
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
   return (
     <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
       <Navbar onOpenCart={() => setCartOpen(true)} />
+        <CartDrawer
+          open={cartOpen}
+          onClose={() => setCartOpen(false)}
+        />
       <main className='py-8'>
         <Container>
           <ProductList />
