921ccd5946df6e028e277fbeb107c59ee9219b60
rabia zia nezami <rabianezami786@gmail.com>
2026-04-27 23:25:33 +0430

    Wrap App with SettingsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { AppProviders } from './app/Providers.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { SettingsProvider } from './features/settings/context/SettingsContext'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AppProviders>
-        <App />
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
       </AppProviders>
     </BrowserRouter>
   </StrictMode>
