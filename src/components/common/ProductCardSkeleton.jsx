fcd558fe8a12b53480cb5f83e043c80e9eb06394
rabia zia nezami <rabianezami786@gmail.com>
2026-04-28 23:43:08 +0430

    feat: implement system-wide dark mode and UI consistency

--- src/components/common/ProductCardSkeleton.jsx
+++ src/components/common/ProductCardSkeleton.jsx
@@ -2,16 +2,16 @@ import { Skeleton } from "../ui/skeleton";
 
 export default function ProductCardSkeleton() {
   return (
-    <div className="animate-pluse space-y-3 border border-slate-200 rounded-xl  p-4">
-      <Skeleton className="h-44 w-full bg-slate-200" />
+    <div className="animate-pulse space-y-3 border border-border rounded-xl  p-4 bg-card">
+      <Skeleton className="h-44 w-full" />
 
-      <Skeleton className="h-4 w-3/4 bg-slate-200" />
+      <Skeleton className="h-4 w-3/4 " />
 
-      <Skeleton className="h-4 w-1/2 bg-slate-200" />
+      <Skeleton className="h-4 w-1/2" />
 
       <div className="flex justify-between">
-          <Skeleton className="h-4 w-25 bg-slate-200" />
-          <Skeleton className="h-4 w-15 bg-slate-200" />
+          <Skeleton className="h-4 w-25 " />
+          <Skeleton className="h-4 w-15 " />
       </div>
 
     </div>
