4eb7f9858cbec7d1cedfe7643d1edee7a9e56bee
rabia zia nezami <rabianezami786@gmail.com>
2026-04-28 19:02:23 +0430

    add color from default tawilend

--- src/components/common/UserAvatar.jsx
+++ src/components/common/UserAvatar.jsx
@@ -3,8 +3,8 @@ import { User } from "lucide-react";
 
 export default function UserAvatar() {
   return (
-    <Avatar className="h-8 w-8 ">
-        <AvatarFallback className="bg-black text-white">
+    <Avatar className="h-8 w-8 cursor-pointer">
+        <AvatarFallback className="bg-slate-950 text-white dark:bg-white dark:text-black border border-border/10">
             <User className="w-4 h-4"/>
         </AvatarFallback>
     </Avatar>
