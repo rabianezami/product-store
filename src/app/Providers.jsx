6222740bb274fc1dab72106e8b60e08ba4bc0e0f
rabia zia nezami <rabianezami786@gmail.com>
2026-04-29 15:12:18 +0430

    build: setup PersistGate for redux-persist

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,13 +1,17 @@
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Provider } from "react-redux"
 import { queryClient } from "./queryClient"
-import { store } from "./store"
+import { store, persistor } from "./store"
+
+import { PersistGate } from "redux-persist/integration/react"
 
 export const AppProviders = ({ children }) => {
     return (
         <QueryClientProvider client={queryClient}>
             <Provider store={store}>
-                {children}
+               <PersistGate loading={null} persistor={persistor}>
+                  {children}
+               </PersistGate>
             </Provider>
         </QueryClientProvider>
     )
