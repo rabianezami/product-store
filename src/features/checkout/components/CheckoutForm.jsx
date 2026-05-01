75cc61b36815a797d184d2047a6dc13252ab6d63
rabia zia nezami <rabianezami786@gmail.com>
2026-05-01 21:49:11 +0430

    feat: add CheckoutForm component

---
+++ src/features/checkout/components/CheckoutForm.jsx
@@ -0,0 +1,72 @@
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { checkoutSchema } from "../validation/checkoutSchema"
+import { useDispatch } from "react-redux"
+import { clearCart } from "@/features/cart/cartSlice"
+import InputField from "@/components/common/form/InputField"
+
+
+export default function CheckoutForm({ setLoading}) {
+    const dispatch = useDispatch()
+
+     const { register, handleSubmit, reset, formState: { errors }, } = useForm({
+        resolver: yupResolver(checkoutSchema),
+        mode: ontouchend,
+    })
+    
+
+    const onSubmit = async (data) => {
+        setLoading(true)
+
+        await new Promise(r => setTimeout(r, 1000))
+
+        console.log("Form Data:", data)
+        toast.success("Order placed successfully")
+
+        dispatch(clearCart())
+        reset()
+        setLoading(false)
+    }
+
+   
+    return (
+        <form id="checkout-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="space-y-4 p-6 bg-card border ">
+                <h2 className="text-xl font-bold text-foreground">
+                   Shipping Details
+                </h2>
+                
+               <div className="grid grid-cols-2 gap-4">
+                <InputField 
+                  label="First Name"
+                  id="firstName"
+                  {...register("firstName")}
+                  error={errors.firstName?.message}
+                />
+
+                <InputField 
+                  label="Last Name"
+                  id="lastName"
+                  {...register("lastName")}
+                  error={errors.lastName?.message}
+                />
+               </div>
+
+               <InputField 
+                label="Address"
+                id="address"
+                {...register("address")}
+                error={errors.address?.message}
+               />
+
+               <InputField
+                label="Phone"
+                id="phone"
+                {...register("phone")}
+                error={errors.phone?.message}
+               />
+            </div>
+        </form>
+    )
+}
