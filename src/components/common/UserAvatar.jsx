36bffa657dd6dee5008b1e2e2837f4adabb1c268
rabia zia nezami <rabianezami786@gmail.com>
2026-05-02 15:36:02 +0430

    add onClick props to UserAvatar

--- src/components/common/UserAvatar.jsx
+++ src/components/common/UserAvatar.jsx
@@ -1,12 +1,15 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { User } from "lucide-react";
 
-export default function UserAvatar() {
+export default function UserAvatar({ onClick }) {
   return (
-    <Avatar className="h-8 w-8 cursor-pointer">
-        <AvatarFallback className="bg-slate-950 text-white dark:bg-white dark:text-black border border-border/10">
-            <User className="w-4 h-4"/>
-        </AvatarFallback>
+    <Avatar 
+      onClick={onClick}
+      className="h-8 w-8 cursor-pointer"
+    >
+      <AvatarFallback className="bg-slate-950 text-white dark:bg-white dark:text-black border border-border/10">
+        <User className="w-4 h-4"/>
+      </AvatarFallback>
     </Avatar>
   );
 }
