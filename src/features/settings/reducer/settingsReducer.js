0b2163fe021115c37b6ce33ba370fd011d159cd7
rabia zia nezami <rabianezami786@gmail.com>
2026-05-03 00:02:58 +0430

    feat: add grid/list view support

--- src/features/settings/reducer/settingsReducer.js
+++ src/features/settings/reducer/settingsReducer.js
@@ -1,13 +1,20 @@
+import { View } from "lucide-react"
+
 const savedTheme = localStorage.getItem("theme") || "system"
 
 export const initialState =  {
-  theme: savedTheme
+  theme: savedTheme,
+  view: "grid" // or list
 }
 
 export function settingsReducer(state, action) {
    switch (action.type) {
      case "SET_THEME": 
        return { ...state, theme: action.payload }
+
+      case "SET_VIEW":
+        return {...state, view: action.payload}
+        
      default:
        return state
    }
